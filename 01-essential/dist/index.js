d75d8a3bc016c3dda78ec11f065de1e307f25d7c
wushuzh <zhangjiaqiang@gmail.com>
2017-12-19 16:28:18 +0800

    use JSX to create header mix with JS

--- 01-essential/dist/index.js
+++ 01-essential/dist/index.js
@@ -1,20 +1,10 @@
-const { createElement } = React
 const { render } = ReactDOM
 
-const style = {
-  backgroundColor: "orange",
-  color: 'white',
-  fontFamily: 'verdana'
-}
-
-const title = createElement(
-  "h1",
-  {id: "title", className: "header", style: style},
-  "Hello World"
-)
-
 render(
-  title,
+  <h1 id="title" className="header"
+    style={{backgroundColor: "orange", color: "white", fontFamily: "verdana"}}>
+    Hello World
+  </h1>,
   document.getElementById("react-container")
 )
 

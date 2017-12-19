71df18f68d046900516407823713151bf8d8bddb
wushuzh <zhangjiaqiang@gmail.com>
2017-12-19 16:06:50 +0800

    use react to get header

---
+++ 01-essential/dist/index.js
@@ -0,0 +1,11 @@
+const title = React.createElement(
+  "h1",
+  {id: "title", className: "header"},
+  "Hello World"
+)
+
+ReactDOM.render(
+  title,
+  document.getElementById("react-container")
+)
+

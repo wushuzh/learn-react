32f0ff1d3d1aded4f3ceffd54f0d892b0ea7ab72
wushuzh <zhangjiaqiang@gmail.com>
2018-01-15 13:11:58 +0800

    use ES6 to create SkiDayCount react component

---
+++ 01-essential/src/components/SkiDayCount.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export class SkiDayCount extends React.Component {
+  render() {
+    return (
+      <div className="ski-day-count">
+        <div className="total-days">
+          <span>5 days</span>
+        </div>
+        <div className="powder-days">
+          <span>2 days</span>
+        </div>
+        <div className="backcountry-days">
+          <span>1 hiking day</span>
+        </div>
+      </div>
+    )
+  }
+}

400aab52e7c1588e7ca0478702db41b96da8d63d
wushuzh <zhangjiaqiang@gmail.com>
2018-01-16 10:02:51 +0800

    add default props value for Count Component

--- 01-essential/src/components/SkiDayCount.js
+++ 01-essential/src/components/SkiDayCount.js
@@ -1,37 +1,49 @@
+import { Component } from 'react'
 import '../stylesheets/ui.scss'
 import Terrain from 'react-icons/lib/md/terrain'
 import SnowFlake from 'react-icons/lib/ti/weather-snow'
 import Calendar from 'react-icons/lib/fa/calendar'
 
-const percentToDecimal = (decimal) => {
+export class SkiDayCount extends Component {
+  percentToDecimal(decimal) {
     return ((decimal * 100) + '%')
+  }
+
+  calcGoalProgress(total, goal) {
+    return this.percentToDecimal(total/goal)
+  }
+
+  render() {
+    return (
+      <div className="ski-day-count">
+        <div className="total-days">
+          <span>{this.props.total}</span>
+            <Calendar />
+          <span>days</span>
+        </div>
+        <div className="powder-days">
+          <span>{this.props.powder}</span>
+            <SnowFlake />
+          <span>days</span>
+        </div>
+        <div className="backcountry-days">
+          <span>{this.props.backcountry}</span>
+            <Terrain />
+          <span>days</span>
+        </div>
+        <div>
+          <span>{this.calcGoalProgress(
+            this.props.total,
+            this.props.goal)}</span>
+        </div>
+      </div>
+    )
+  }
 }
 
-const calcGoalProgress = (total, goal) => {
-    return percentToDecimal(total/goal)
+SkiDayCount.defaultProps = {
+  total: 50,
+  powder: 10,
+  backcountry: 15,
+  goal: 75
 }
-
-export const SkiDayCount = ({total, powder, backcountry, goal}) => (
-  <div className="ski-day-count">
-    <div className="total-days">
-      <span>{total}</span>
-        <Calendar />
-      <span>days</span>
-    </div>
-    <div className="powder-days">
-      <span>{powder}</span>
-        <SnowFlake />
-      <span>days</span>
-    </div>
-    <div className="backcountry-days">
-      <span>{backcountry}</span>
-        <Terrain />
-      <span>days</span>
-    </div>
-    <div>
-      <span>{calcGoalProgress(
-        total,
-        goal)}</span>
-    </div>
-  </div>
-)
--- 01-essential/src/index.js
+++ 01-essential/src/index.js
@@ -1,32 +1,36 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { SkiDayList } from './components/SkiDayList'
+import { SkiDayCount } from './components/SkiDayCount'
 
 window.React = React
 
 render(
-  <SkiDayList days={
-    [
-      {
-        resort: "Squaw Valley",
-        date: new Date("1/15/2018"),
-        powder: true,
-        backcountry: false
-      },
-      {
-        resort: "Kirkwood",
-        date: new Date("3/29/2017"),
-        powder: false,
-        backcountry: false
-      },
-      {
-        resort: "Mt. Tallac",
-        date: new Date("4/2/2017"),
-        powder: false,
-        backcountry: true
-      }
-    ]}
-  />,
+  <div>
+    <SkiDayCount />
+    <SkiDayList days={
+      [
+        {
+          resort: "Squaw Valley",
+          date: new Date("1/15/2018"),
+          powder: true,
+          backcountry: false
+        },
+        {
+          resort: "Kirkwood",
+          date: new Date("3/29/2017"),
+          powder: false,
+          backcountry: false
+        },
+        {
+          resort: "Mt. Tallac",
+          date: new Date("4/2/2017"),
+          powder: false,
+          backcountry: true
+        }
+      ]}
+    />
+  </div>,
   document.getElementById("react-container")
 )
 
